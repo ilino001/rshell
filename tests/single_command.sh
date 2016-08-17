4411d69b07afde7c14ebf875772b6a32d6f0bc38
Isaac Lino <ilino@hammer.cs.ucr.edu>
2016-08-16 22:54:14 -0700

    removed comments on single command test

--- tests/single_command.sh
+++ tests/single_command.sh
@@ -1,10 +1,9 @@
 echo "testing commented command..."
-echo commands # comments with commands 
-ls -a || echo testing with multiple commands # testing comments on multiple commands
-echo testing... # ls commands
-mkdir directory # create a directory
-rm -rf directory # remove a directory
-echo mkdir directory # echo directory
+echo commands 
+echo testing...
+mkdir directory
+rm -rf directory
+echo mkdir directory
 touch file.cpp
-rm file.cpp # remove 
+rm file.cpp  
 exit
