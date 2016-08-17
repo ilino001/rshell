7d0a56cee504a8a06baf05b01435fe8f2b405d4f
Isaac Lino <ilino@hammer.cs.ucr.edu>
2016-08-16 23:05:45 -0700

    connector added

--- src/Connector.h
+++ src/Connector.h
@@ -5,17 +5,20 @@
 
 class Connector: public Base {
     protected:
+        //left and right child 
         Base* left;
         Base* right;
     public:
+        //sets left and right default to NULl 
         Connector() {
             left = NULL;
             right = NULL; 
         }
+        //sets parameters to left and right children 
         Connector(Base *l, Base *r) {
             left = l; 
             right = l; 
         }
-        virtual bool execute() = 0;
+        virtual bool execute(int child, int parent) = 0;
 }; 
 #endif 
