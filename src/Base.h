222fa51fd35cab470d5f27ca5ebfff001ff90192
Isaac Lino <ilino@hammer.cs.ucr.edu>
2016-08-16 20:20:42 -0700

    Header files Base and Command has been updated

--- src/Base.h
+++ src/Base.h
@@ -1,20 +1,19 @@
 #ifndef _BASE_H_
 #define _BASE_H_
-#include <vector>
 
 class Base {
-    private:
-        std::list<Base*> tree; 
+    // private:
+        // std::list<Base*> tree; 
     public:
-        Base() : tree(NULL); 
-        virtual bool execute() = 0;
-        virtual void display() = 0;
-        void add(Base* element) {
-            tree.push_back(element);
-        }
-        void remove(Base* element) {  
-            tree.remove(element);   
-        }    
+        Base() {} 
+        virtual bool execute(int child, int parent) = 0;
+        //virtual void display() = 0;
+        // void add(Base* element) {
+        //     tree.push_back(element);
+        // }
+        // void remove(Base* element) {  
+        //     tree.remove(element);   
+        // }    
 };  
 #endif 
              
--- src/Command.h
+++ src/Command.h
@@ -4,22 +4,69 @@
 #include "Base.h"
 #include <iostream> 
 #include <string> 
+#include <unistd.h> 
+#include <sys/wait.h>
+#include <stdio.h>
 
 class Command: public Base {
-    private:
-        std::string comm;
-        
+    protected: 
+        std::string command;
     public:
-        Command(std::string c) {
-            comm = c;
+        //constructors 
+        Command() {} 
+        Command(std::string com) {
+            command = com;
         }
         
-        bool execute() {
-
+        //uses fork in system calls 
+        virtual bool execute(int child, int parent) {
+            
+            pid_t pid = fork();     //creating child process 
+            
+            //fork fails, output error 
+            if (pid == -1) { 
+                perror("ERROR: fork() failed, cannot continue\n"); 
+                exit(1); 
+            }
+            
+            //child process 
+            else if (pid == 0){
+                //uses dup2 for 
+                if (dup2(child, 0) == -1){
+                    perror("ERROR: dup2 failed, cannot continue\n"); 
+                    return false;
+                }
+                
+                if (dup2(parent, 1) == -1){
+                    perror("ERROR: dup2 failed, cannot continue\n"); 
+                    return false;
+                }
+                
+                
+            }
+            
+            //parent process 
+            else if (pid > 0) {
+                //waitpid 
+                int verify;
+                
+              if (waitpid(pid, &verify, 0) == -1){
+                  perror("Waiting...");
+              }  
+              if(WEXITSTATUS(verify) != 0){
+                  return false;
+              }
+                
+            }
+            
+            //fork fails, causes error 
+            else {    
+                perror("ERROR: cannot continue\n"); 
+                exit(1); 
+            }
+            
             return false;
         }
-        void display() {
-            std::cout << comm;
-        }
+
 }; 
 #endif 
