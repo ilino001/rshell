5393a9aa201b97f59db2117dc647179201a208db
Isaac Lino <ilino@hammer.cs.ucr.edu>
2016-08-16 21:02:06 -0700

    include tests cases comments

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,7 +1,7 @@
 echo "testing exit..."
-#exit                    # plain exit 
-echo command && exit    # exit after command using &&
-echo command || exit    # exit after command using ||
-echo command;           # exit after command by ; without spaces
+exit
+echo command && exit # exit after command using &&
+echo command || exit # exit after command using ||
+echo command; # exit after command by ; without spaces
 
 #../bin/rshell < test_exit
