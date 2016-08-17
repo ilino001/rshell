dea7a6957fd34caaeaecd27ee7034ca7dc0adb64
Isaac Lino <ilino@hammer.cs.ucr.edu>
2016-08-16 23:01:12 -0700

    no comments on exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,5 +1,5 @@
 echo "testing exit..."
 exit
-echo command && exit # exit after command using &&
-echo command || exit # exit after command using ||
-echo command; # exit after command by ; without spaces
+echo command && exit
+echo command || exit
+echo command;
