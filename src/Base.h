6a1a88cfa373c1c8032565a913a033fb767d5162
Isaac Lino <ilino@hammer.cs.ucr.edu>
2016-08-16 23:03:54 -0700

    Base.h modified

--- src/Base.h
+++ src/Base.h
@@ -2,18 +2,9 @@
 #define _BASE_H_
 
 class Base {
-    // private:
-        // std::list<Base*> tree; 
     public:
         Base() {} 
         virtual bool execute(int child, int parent) = 0;
-        //virtual void display() = 0;
-        // void add(Base* element) {
-        //     tree.push_back(element);
-        // }
-        // void remove(Base* element) {  
-        //     tree.remove(element);   
-        // }    
 };  
 #endif 
              
