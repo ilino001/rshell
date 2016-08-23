836e2560391afaa722a6760367c6aea4652c8f11
natesong714 <nsong002@ucr.edu>
2016-08-22 17:36:53 -0700

    Cleaned up code and fixed tree

--- src/main.cpp
+++ src/main.cpp
@@ -16,28 +16,20 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
       
-  // extra credit      
-  //
-  //      char *user = getlogin();            
-  //      char host[1000];
-  //      
-  //      gethostname(host, sizeof host); 
-  //      cout << user << "@" <<  host << "$ ";   
-        
-    string cmd[100];
-    string input; 
-    string input2;
-    char cline[100], *command; 
-    char strDelims[] = ";|&#\n"; 
-    int x = 0; 
-    
-    
+    string cmd[100]; //Array for storing all of the commands
+    string cnct[100]; //Array for storing all of the connectors
+    string input; //Input parsed for commands
+    char cline[100]; //Used for conversion from string to char
+    char *command; //The result of the parsed input
+    char strDelims[] = ";|&#\n";  //These characters tell strtok when to parse
+    int x = 0; //Index for the command array
+    int z = 0; //Index for the connector array
+
+    //Opens the file and checks if is_open() fails
     ifstream inFS;
     string filename = argv[1]; //sets second argument to filename 
     //string filename = "../tests/exit.sh"
-    inFS.open(filename.c_str()); //opens filename passed in 
-    
-    //if filename cannot be opened 
+    inFS.open(filename.c_str());
     if (!inFS.is_open()) {
         cout << "ERROR: cannot open " << argv[1]; 
         return 0; 
@@ -45,139 +37,142 @@ int main(int argc, char *argv[]) {
     
     else {
         
-        //while inFS isn't at the end of file 
         while (!inFS.eof()) {
-        x = 0;
-        getline(inFS, input); //reads in each line from file 
-        
-        //if x does not equal zero, it copies input to input2 
-        input2 = input;
-
-        
-        strcpy(cline, input.c_str()); //copies from string to char 
-        command = strtok(cline, strDelims); //copies cline to command, does not takes into consideration the symbols 
+            x = 0;
+            z = 0;
+            getline(inFS, input); //Reads in each line from file
+            //Checks for any comments and resizes before comment happens
+            for (unsigned i = 0; i < input.length(); ++i) {
+                if (input.at(i) == '#') {
+                    input.resize(i);
+                }
+            }
+            if (input == "") {
+                exit(0);
+            }
+            strcpy(cline, input.c_str()); //Copies the input into char for strtok
+            command = strtok(cline, strDelims); //Parses cline to isolate commands
   
-        while (command != NULL) {
-            //if command does not equal, insert into string cmd 
-            if (command[0] != '#') {
-              cmd[x] = command; //insert current command into string 
-              cout << "Command at " << x <<  " is: " << cmd[x] << endl;
+            while (command != NULL) {
+                  cmd[x] = command; //Inserts command into cmd array
+                  cout << "Command at " << x <<  " is: " << cmd[x] << endl;
+            
+                //Continues onto next command
+                command = strtok(NULL, strDelims); 
+                ++x; 
+            }
+  
+            strcpy(cline, input.c_str()); //Copies the input into char for strtok
+            command = strtok(cline, " "); //Parses cline up to the first space
+    
+            while (command != NULL) { 
+              //checks all the parsed words, char by char.
+              //checks for any of these symbols: ; && and ||
+              //if the symbol is found, add it to the connector array and increment counter
+                for (unsigned int i = 0; i < strlen(command); ++i) { //iterates til end of parsed word
+                    if (command[i] == ';') {
+                        cnct[z] = command[i];
+                        cout << "Connector Array at " << z << " is: " << cnct[z] << endl;
+                        ++z;
+                    }  
+                    if (command[i] == '&' && command[i+1] == '&') {
+                        cnct[z] = command[i];
+                        cout << "Connector Array at " << z << " is: " << cnct[z] << endl;
+                        ++z;
+                    }
+                    if (command[i] == '|' && command[i+1] == '|') {
+                        cnct[z] = command[i];
+                        cout << "Connector Array at " << z << " is: " << cnct[z] << endl;
+                        ++z;
+                    }
+                }
+            command = strtok(NULL, " "); //Moves on to the next word seperated by space
             }
             
-            //continues to next char 
-            command = strtok(NULL, strDelims); 
-            ++x; //increment x 
-        }
-        
-
-        
-        int z = 0; //index for the connector array
-        char dline[100]; //used for conversion from string to char
-        char *command2; //the result after parsing
-        string cnct [100]; //connector array
-        strcpy(dline, input2.c_str()); //converts input into char array
-        command2 = strtok(dline, " "); //parses dline up to the first space
     
-        while (command2 != NULL) { 
-          //checks all the parsed words, char by char.
-          //checks for any of these symbols: ; && and ||
-          //if the symbol is found, add it to the connector array and increment counter
-        for (unsigned int i = 0; i < strlen(command2); ++i) { //iterates til end of parsed word
-          if (command2[i] == ';') {
-            cnct[z] = command2[i];
-            cout << "Connector Array at " << z << " is: " << cnct[z] << endl;
-            ++z;
-          } 
-          if (command2[i] == '&' && command2[i+1] == '&') {
-            cnct[z] = command2[i];
-            cout << "Connector Array at " << z << " is: " << cnct[z] << endl;
-            ++z;
-          }
-          if (command2[i] == '|' && command2[i+1] == '|') {
-            cnct[z] = command2[i];
-            cout << "Connector Array at " << z << " is: " << cnct[z] << endl;
-            ++z;
-          }
-      }
-      command2 = strtok(NULL, " "); //Moves on to the next word seperated by space
-  }
-  //ARRAY CMD POPULATED WITH ALL THE COMMANDS
-  //ARRAY CNCT POPULATED WITH ALL THE CONNECTORS
-  
-  vector<Base*> commandVector; //vector for all commands
-  for (int j = 0; j < x; ++j) {
-    Base* comm;   //default for command to be added 
-      if (cmd[j] == "exit") { //if the command is exit, declare as Exit object
-        comm = new Exit();
-      }
-      else {    //if command is not exit, declare as regular command object
-        comm = new Command(cmd[j]);
-      }
-      commandVector.push_back(comm);  //add command to vector
-  }
-  
-  
-  vector<Base*> connectorVector;
-  int i = z - 1; //highest index of connectors
-  int j = x - 1; //highest index of  commands
+    
+            vector<Base*> commandVector; //Vector for all commands
+            vector<Base*> connectorVector; //Vector for all connectors
+            
+            if (x != 0) {
+                for (int j = 0; j < x; ++j) {
+                    Base* comm;   
+                    if (cmd[j] == "exit") { //If the command is exit, declare Exit object
+                        comm = new Exit();
+                    }
+                    else {  //If command is not exit, declare regular command object
+                        comm = new Command(cmd[j]);
+                    }
+                    commandVector.push_back(comm); //Add command to commandVector
+                }
+            }
+            
+            cout << "number of commands " << x << endl;
+            cout << "number of connectors " << z << endl;
+           
+            if (z != 0) {
+                 
+                int i = z - 1; //Highest index of connectors
+                int j = x - 1; //Highest index of commands
+    
+                    //in the declaration of the last connector in the array
+                //decrement j since you already used 2 of the commands in the treee
+ 1               //decrement i since you just used the 1 of the connectors in the tree
+                while (i >= 0 && j >= 0) { //Continue as long as we don't run out of connecters or commands
+                    if (cnct[i] == ";") {
+                        Semicolon* A = 0;
+                        if (i == z - 1) {
+                            A = new Semicolon(commandVector.at(j-1), commandVector.at(j));
+                            --j;
+                        }
+                        else {
+                            A = new Semicolon(commandVector.at(j), connectorVector.at(connectorVector.size()-1));
+                        }
+                        --j;
+                        --i;    
+                        connectorVector.push_back(A);
 
-  //Create the first part of the tree, using the last two commands in the array as parameters
-  //in the declaration of the last connector in the array
-  //decrement j since you already used 2 of the commands in the treee
-  //decrement i since you just used the 1 of the connectors in the tree
-    if (cnct[i] == ";") {
-        Semicolon* A = new Semicolon(commandVector.at(j-1), commandVector.at(j));
-        connectorVector.push_back(A);
-        --j;
-        --j;
-        --i;
-    }
-    if (cnct[i] == "|") {
-        Or* B = new Or(commandVector.at(j-1), commandVector.at(j));
-        connectorVector.push_back(B);
-        --j;
-        --j;
-        --i;
-    }
-    if (cnct[i] == "&") {
-        And* C = new And(commandVector.at(j-1), commandVector.at(j));
-        connectorVector.push_back(C);
-        --j;
-        --j;
-        --i;
-    }
-  
+                    }
+                    else if (cnct[i] == "|") {
+                        Or* B = 0;
 
-  //Checks which connector is in the cnct array to decide which contructor to call
-  //Now the right child of every connector will be another connecter
-  //The left child will be a command
-  //Decrement j and i after you add a signle connector and command to the tree
-  while (i >= 0 && j >= 0) { //Continue as long as we don't run out of connecters or commands
-    if (cnct[i] == ";") {
-        Semicolon* A = new Semicolon(commandVector.at(j), connectorVector.at(connectorVector.size()-1));
-        connectorVector.push_back(A);
-        --j;
-        --i;
-    }
-    if (cnct[i] == "|") {
-        Or* B = new Or(commandVector.at(j), connectorVector.at(connectorVector.size()-1));
-        connectorVector.push_back(B);
-        --j;
-        --i;
-    }
-    if (cnct[i] == "&") {
-        And* C = new And(commandVector.at(j), connectorVector.at(connectorVector.size()-1));
-        connectorVector.push_back(C);
-        --j;
-        --i;
-    }
-  }
+                        if (i == z - 1) {
+                            B = new Or(commandVector.at(j-1), commandVector.at(j));
+                            --j;
+                        }
+                        else {
+                            B = new Or(commandVector.at(j), connectorVector.at(connectorVector.size()-1));
+                        }
+                        --j;
+                        --i;
+                        connectorVector.push_back(B);
+                    }
+                    else if (cnct[i] == "&") {
+                        And* C = 0;
+                        if (i == z - 1) {
+                            C = new And(commandVector.at(j-1), commandVector.at(j));
+                            --j;
+                        }
+                        else {
+                            C = new And(commandVector.at(j), connectorVector.at(connectorVector.size()-1));
+                        }
+                        --j;
+                        --i;
+                        connectorVector.push_back(C);
+                    }
+                }
+            }
+            
+            cout << "Connector Vector size: " << connectorVector.size() << endl;
+            cout << "----------------------------------" << endl;
 
-//connectorVector.at(connectorVector.size() - 1)->execute(0, 1);
-// connectorVector.at(connectorVector.size() - 1); 
-//executing the root of the tree recursively executes its children
+            if (z == 0 && x == 1) {
+                commandVector.at(commandVector.size() - 1)->execute(0, 1);
+            }
+            else {
+                connectorVector.at(connectorVector.size() - 1)->execute(0, 1);
+            }
         }
-       
     } 
 
     return 0; 
