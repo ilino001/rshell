7a54715ed6dbef245c66920b07b6b9f5e7c6e56e
Isaac Lino <ilino@hammer.cs.ucr.edu>
2016-08-25 20:03:20 -0700

    commented_command.sh script implemented

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -5,3 +5,6 @@ echo words1 && echo words2 #&& echo word
 echo word1 #;  echo words
 echo word2 #&& echo words
 echo word3 #|| echo words
+ls -a || echo world #sample
+ls -j || echo world #j flag
+echo "hello world # with commnets"
