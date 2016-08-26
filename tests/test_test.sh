36e547e342254d7647c783f709c9e1c4f5f208e2
ilino001 <ilino001@ucr.edu>
2016-08-26 13:58:18 -0700

    Add files via upload
    test cases

---
+++ tests/test_test.sh
@@ -0,0 +1,20 @@
+echo "test src"
+test src
+
+echo "[ whaterver ]"
+[ whatever ]
+
+echo "testing..."
+test testing...
+
+echo "[ -e src ]"
+[ -e src ]
+
+echo "[ -f src ]"
+[ -f src ]
+
+echo "[ -d src ]"
+[ -d src ]
+
+echo "test -f src/main.cpp"
+test -f src/main.cpp
