289b8fd35eab7217c1e1c16501df51429581ffb0
Isaac Lino <ilino@hammer.cs.ucr.edu>
2016-08-25 23:34:49 -0700

    main.cpp added

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <sstream>
+#include <unistd.h>
+#include "Base.h"
+#include "Connector.h"
+#include "Command.h"
+#include "Precedence.h"
+#include "Parse_Cmd.h"
+
+using namespace std;
+int main() {
+    
+    Parse_Cmd parser;
+
+    cout << "\nwelcome to rshell!" << endl;
+  
+    for ( ; ; )
+    {
+         cout << "> ";
+         string str;
+         getline(cin, str); 
+        
+        stringstream input(str);
+         
+        Base* cmd;
+        cmd = parser.parse(input);
+        
+        // cmd->execute(); 
+        //  delete cmd;
+         
+    }
+    
+   return 0;
+}
